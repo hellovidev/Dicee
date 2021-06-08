bce2295769ea20ef24049eaf615088b6ac6980cb
Hello VI Dev <79969749+hellovidev@users.noreply.github.com>
2021-06-08 15:51:30 +0300

    Added Assets

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UI elements
+    
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstDice.alpha = 0.5
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func rollClick(_ sender: UIButton) {
+        firstDice.image = #imageLiteral(resourceName: "DiceSix")
+        secondDice.image = #imageLiteral(resourceName: "DiceThree")
+    }
+    
 
 }
 
