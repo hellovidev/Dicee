eb5378a49779b3026caf338d8f53d1e01da670c7
Hello VI Dev <79969749+hellovidev@users.noreply.github.com>
2021-06-08 16:34:34 +0300

    Added Random

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondDice: UIImageView!
     @IBOutlet weak var rollButton: UIButton!
     
+    let array = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        firstDice.alpha = 0.5
+        firstDice.alpha = 0.9
+        rollButton.layer.cornerRadius = 6
         // Do any additional setup after loading the view.
     }
     
     @IBAction func rollClick(_ sender: UIButton) {
-        firstDice.image = #imageLiteral(resourceName: "DiceSix")
-        secondDice.image = #imageLiteral(resourceName: "DiceThree")
+        firstDice.image = array[Int.random(in: 0..<6)]
+        secondDice.image = array[Int.random(in: 0..<6)]
     }
     
 
